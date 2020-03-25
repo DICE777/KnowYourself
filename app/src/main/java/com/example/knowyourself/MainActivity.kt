b0e8d825b531f3a823577d3a805e750a3f8f26c8
DICE777 <seulgi7383@gmail.com>
2020-03-25 23:44:10 +0900

    Dialog Test

--- app/src/main/java/com/example/knowyourself/MainActivity.kt
+++ app/src/main/java/com/example/knowyourself/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.knowyourself
 
+import android.app.Dialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val alertDialog: AlertDialog? = applicationContext?.let {
+            val builder = AlertDialog.Builder(this)
+            builder.apply {
+                setPositiveButton("ok",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User clicked OK button
+                    })
+                setNegativeButton("cancel",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        // User cancelled the dialog
+                    })
+            }
+            // Set other dialog properties
+
+            // Create the AlertDialog
+            builder.create()
+        }
+
+        alertDialog?.show();
     }
+
 }
